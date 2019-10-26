de3fba762ae583805efdb745c86f32fe6e8fc736
Dimitrios Ellinidis <isendil_1@yahoo.com>
2019-10-26 12:22:03 -0400

    2.8.1 done

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-//const Movies = require("./movies");
 const morgan = require("morgan");
 const bodyParser = require("body-parser"),
   methodOverride = require("method-override");
@@ -23,33 +22,12 @@ app.use(methodOverride());
 app.use(function(err, req, res, next) {
   // logic
 });
-// var myLogger = function(req, res, next) {
-//   req.url;
-//   console.log(req.url);
-
-//   next();
-// };
 app.use(morgan("common"));
-//app.use(myLogger);
 app.use(express.static("public"));
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
-let topBooks = [
-  {
-    title: "Harry Potter and the Sorcerer's Stone",
-    author: "J.K. Rowling"
-  },
-  {
-    title: "Lord of the Rings",
-    author: "J.R.R. Tolkien"
-  },
-  {
-    title: "Twilight",
-    author: "Stephanie Meyer"
-  }
-];
 
 // GET requests
 app.get("/", function(req, res) {
@@ -63,18 +41,10 @@ app.get("/secreturl", function(req, res) {
   res.send(text);
 });
 app.get("/documentation.html");
-app.get("/books", function(req, res) {
-  res.json(topBooks);
-});
-// app.get("/movies", function(req, res) {
-//   res.json(Movies);
-// });
 app.get("/movies", function(req, res) {
   Movies.find().then(movies => res.json(movies));
 });
-// app.get("/users", function(req, res) {
-//   Users.find().then(users => res.json(users));
-// });
+
 // Get all users
 app.get("/users", function(req, res) {
   Users.find()
