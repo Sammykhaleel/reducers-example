00153f863f40bc9bcbd88b6066e84df70f578e1e
Isendil <isendil_1@yahoo.com>
2020-01-25 11:20:06 -0500

    commiting from laptop

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -52659,7 +52659,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55357" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50377" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
